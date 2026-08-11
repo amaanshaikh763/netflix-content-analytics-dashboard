deb4c902aaefaa1dcfeb01c2250d175c618f3f34
amaan-data-analyst <amaanshaikh763@gmail.com>
2026-08-11 23:46:28 +0530

    Create data import script for Netflix Titles dataset
    Added SQL script for importing Netflix Titles dataset.

---
+++ Netflix-Content-Strategy-Dashboard/sql/02_data_import.sql
@@ -0,0 +1,26 @@
+/*
+=========================================
+DATA IMPORT
+=========================================
+
+Dataset:
+Netflix Titles Dataset
+
+Source:
+CSV File
+
+Import Method:
+pgAdmin 4 Import/Export Wizard
+
+Delimiter:
+Comma (,)
+
+Header:
+Yes
+
+Encoding:
+UTF-8
+
+Rows Imported:
+8807
+*/
